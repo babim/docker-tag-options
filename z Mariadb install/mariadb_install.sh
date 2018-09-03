9a339015bad27773052052f9b2e75bf8d4522b6a
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-04 02:33:08 +0700

    1

--- z Mariadb install/mariadb_install.sh
+++ z Mariadb install/mariadb_install.sh
@@ -6,6 +6,62 @@ fi
 echo 'Check OS'
 if [[ -f /etc/debian_version ]]; then
 
+# install ssl
+apt-get install -y --no-install-recommends wget apt-transport-https ca-certificates gpg
+
+# add gosu for easy step-down from root
+export GOSU_VERSION=1.10
+set -ex; \
+	dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"; \
+	wget --no-check-certificate --progress=bar:force -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch"; \
+	chmod +x /usr/local/bin/gosu; \
+# verify that the binary works
+	gosu nobody true
+
+# add repo Mariadb
+set -ex; \
+	key='199369E5404BD5FC7D2FE43BCBCB082A1BB943DB'; \
+	export GNUPGHOME="$(mktemp -d)"; \
+	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
+	gpg --export "$key" > /etc/apt/trusted.gpg.d/mariadb.gpg; \
+	command -v gpgconf > /dev/null && gpgconf --kill all || :; \
+	rm -rf "$GNUPGHOME"; \
+	apt-key list > /dev/null
+	key='430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A'; \
+	export GNUPGHOME="$(mktemp -d)"; \
+	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
+	gpg --export "$key" > /etc/apt/trusted.gpg.d/mariadb.gpg; \
+	command -v gpgconf > /dev/null && gpgconf --kill all || :; \
+	rm -rf "$GNUPGHOME"; \
+	apt-key list > /dev/null
+	key='4D1BB29D63D98E422B2113B19334A25F8507EFA5'; \
+	export GNUPGHOME="$(mktemp -d)"; \
+	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
+	gpg --export "$key" > /etc/apt/trusted.gpg.d/mariadb.gpg; \
+	command -v gpgconf > /dev/null && gpgconf --kill all || :; \
+	rm -rf "$GNUPGHOME"; \
+	apt-key list > /dev/null
+
+# add Percona's repo for xtrabackup (which is useful for Galera)
+echo "deb https://repo.percona.com/apt $OSDEB main" > /etc/apt/sources.list.d/percona.list \
+	&& { \
+		echo 'Package: *'; \
+		echo 'Pin: release o=Percona Development Team'; \
+		echo 'Pin-Priority: 998'; \
+	} > /etc/apt/preferences.d/percona
+
+# add repo Mysql
+set -ex; \
+# gpg: key 5072E1F5: public key "MySQL Release Engineering <mysql-build@oss.oracle.com>" imported
+	key='A4A9406876FCBD3C456770C88C718D3B5072E1F5'; \
+	export GNUPGHOME="$(mktemp -d)"; \
+	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
+	gpg --export "$key" > /etc/apt/trusted.gpg.d/mysql.gpg; \
+	rm -rf "$GNUPGHOME"; \
+	apt-key list > /dev/null
+
+mkdir /docker-entrypoint-initdb.d
+
 # set version
 #export MARIADB_MAJOR=10.0
 
