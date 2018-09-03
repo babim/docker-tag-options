b5296ab23a90b0843e903b524e2f0fa9e80849b0
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-04 03:25:32 +0700

    1

--- z Mariadb install/mariadb_install.sh
+++ z Mariadb install/mariadb_install.sh
@@ -19,24 +19,21 @@ set -ex; \
 	export GNUPGHOME="$(mktemp -d)"; \
 	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
 	gpg --export "$key" > /etc/apt/trusted.gpg.d/mariadb.gpg; \
-	command -v gpgconf > /dev/null && gpgconf --kill all || :; \
 	rm -rf "$GNUPGHOME"; \
 	key='177F4010FE56CA3336300305F1656F24C74CD1D8'; \
 	export GNUPGHOME="$(mktemp -d)"; \
 	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
-	gpg --export "$key" > /etc/apt/trusted.gpg.d/mariadb.gpg; \
-	command -v gpgconf > /dev/null && gpgconf --kill all || :; \
+	gpg --export "$key" >> /etc/apt/trusted.gpg.d/mariadb.gpg; \
 	rm -rf "$GNUPGHOME"; \
 	key='430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A'; \
 	export GNUPGHOME="$(mktemp -d)"; \
 	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
-	gpg --export "$key" > /etc/apt/trusted.gpg.d/mariadb.gpg; \
-	command -v gpgconf > /dev/null && gpgconf --kill all || :; \
+	gpg --export "$key" >> /etc/apt/trusted.gpg.d/mariadb.gpg; \
 	rm -rf "$GNUPGHOME"; \
 	key='4D1BB29D63D98E422B2113B19334A25F8507EFA5'; \
 	export GNUPGHOME="$(mktemp -d)"; \
 	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
-	gpg --export "$key" > /etc/apt/trusted.gpg.d/mariadb.gpg; \
+	gpg --export "$key" >> /etc/apt/trusted.gpg.d/mariadb.gpg; \
 	command -v gpgconf > /dev/null && gpgconf --kill all || :; \
 	rm -rf "$GNUPGHOME"; \
 	apt-key list > /dev/null
