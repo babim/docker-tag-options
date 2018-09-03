20b2bc37699131dfaf9702703510ac3c3f8c1713
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-04 04:05:16 +0700

    1

--- z PHP install/php-repo-ubuntu.sh
+++ z PHP install/php-repo-ubuntu.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#  ____        _     _
+# | __ )  __ _| |__ (_)_ __ ___
+# |  _ \ / _` | '_ \| | '_ ` _ \
+# | |_) | (_| | |_) | | | | | | |
+# |____/ \__,_|_.__/|_|_| |_| |_|
+
 echo 'Check OS'
 if [[ -f /etc/lsb-release ]]; then
 add-apt-repository ppa:ondrej/php -y
--- z PHP install/php7.0.sh
+++ z PHP install/php7.0.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#  ____        _     _
+# | __ )  __ _| |__ (_)_ __ ___
+# |  _ \ / _` | '_ \| | '_ ` _ \
+# | |_) | (_| | |_) | | | | | | |
+# |____/ \__,_|_.__/|_|_| |_| |_|
+
 echo 'Check root'
 if [ "x$(id -u)" != 'x0' ]; then
     echo 'Error: this script can only be executed by root'
