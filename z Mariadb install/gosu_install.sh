7137966739b3b02f4bce0b596e0cd9caf968dfab
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-05 18:04:42 +0700

    1

--- z Mariadb install/gosu_install.sh
+++ z Mariadb install/gosu_install.sh
@@ -1,6 +1,6 @@
 # add gosu for easy step-down from root
 GOSU_VERSION=1.10
-RUN set -ex; \
+set -ex; \
 	dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"; \
 	wget --no-check-certificate --progress=bar:force -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch"; \
 	chmod +x /usr/local/bin/gosu; \
--- z OracleDatabase install/gosu_install.sh
+++ z OracleDatabase install/gosu_install.sh
@@ -1,7 +1,19 @@
 # add gosu for easy step-down from root
 GOSU_VERSION=1.10
-RUN set -ex; \
+set -ex; \
+	yum -y install epel-release; \
+	yum -y install wget dpkg; \
+	\
 	dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"; \
-	wget --no-check-certificate --progress=bar:force -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch"; \
-	chmod +x /usr/local/bin/gosu; \
-	gosu nobody true
+	wget -O /usr/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch"; \
+	wget -O /tmp/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch.asc"; \
+	\
+# verify the signature
+	export GNUPGHOME="$(mktemp -d)"; \
+	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4; \
+	gpg --batch --verify /tmp/gosu.asc /usr/bin/gosu; \
+	rm -r "$GNUPGHOME" /tmp/gosu.asc; \
+	\
+	chmod +x /usr/bin/gosu; \
+# verify that the binary works
+	gosu nobody true;
