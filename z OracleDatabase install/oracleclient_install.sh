75988fb6a2d6934b9f46d891b65e3df867aa396c
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-07 02:21:23 +0700

    1

---
+++ z OracleDatabase install/oracleclient_install.sh
@@ -0,0 +1,9 @@
+cd /tmp && \
+     wget http://media.matmagoc.com/oracle/oracle-instantclient12.2-basic-12.2.0.1.0-1.x86_64.rpm && \
+     wget http://media.matmagoc.com/oracle/oracle-instantclient12.2-devel-12.2.0.1.0-1.x86_64.rpm && \
+     wget http://media.matmagoc.com/oracle/oracle-instantclient12.2-sqlplus-12.2.0.1.0-1.x86_64.rpm && \
+     yum -y install /tmp/oracle-instantclient*.rpm && \
+     yum clean all && \
+     rm -f /tmp/oracle-instantclient*.rpm && \
+     echo /usr/lib/oracle/12.2/client64/lib > /etc/ld.so.conf.d/oracle-instantclient12.2.conf && \
+     ldconfig
