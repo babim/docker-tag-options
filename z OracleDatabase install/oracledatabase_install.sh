44bd8df18a3cc225ed1b86c6caed3a46c9422179
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-07 08:58:06 +0700

    1

--- z OracleDatabase install/oracledatabase_install.sh
+++ z OracleDatabase install/oracledatabase_install.sh
@@ -23,26 +23,28 @@ if [ -f /etc/redhat-release ]; then
 		export CODE=${CODE:-"preinstall-18c"}
 	fi
 	# set environment
-	export ORACLE_BASE=${ORACLE_BASE:-"/opt/oracle"} \
-	export ORACLE_HOME=${ORACLE_HOME:-"$ORACLE_BASE/product/$VERSION/dbhome_1"} \
-	export INSTALL_RSP="db_inst.rsp" \
-	export CONFIG_RSP="dbca.rsp.tmpl" \
-	export PWD_FILE="setPassword.sh" \
-	export RUN_FILE="runOracle.sh" \
-	export START_FILE="startDB.sh" \
-	export CREATE_DB_FILE="createDB.sh" \
-	export SETUP_LINUX_FILE="setupLinuxEnv.sh" \
-	export CHECK_SPACE_FILE="checkSpace.sh" \
-	export CHECK_DB_FILE="checkDBStatus.sh" \
-	export USER_SCRIPTS_FILE="runUserScripts.sh" \
+	echo "set environment"
+#	export ORACLE_BASE=${ORACLE_BASE:-"/opt/oracle"}
+#	export ORACLE_HOME=${ORACLE_HOME:-"$ORACLE_BASE/product/$VERSION/dbhome_1"}
+	export INSTALL_RSP="db_inst.rsp"
+	export CONFIG_RSP="dbca.rsp.tmpl"
+	export PWD_FILE="setPassword.sh"
+	export RUN_FILE="runOracle.sh"
+	export START_FILE="startDB.sh"
+	export CREATE_DB_FILE="createDB.sh"
+	export SETUP_LINUX_FILE="setupLinuxEnv.sh"
+	export CHECK_SPACE_FILE="checkSpace.sh"
+	export CHECK_DB_FILE="checkDBStatus.sh"
+	export USER_SCRIPTS_FILE="runUserScripts.sh"
 	export INSTALL_DB_BINARIES_FILE="installDBBinaries.sh"
 	# Use second ENV so that variable get substituted
-	export INSTALL_DIR=${INSTALL_DIR:-"$ORACLE_BASE/install"} \
-	export PATH=$ORACLE_HOME/bin:$ORACLE_HOME/OPatch/:/usr/sbin:$PATH \
-	export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/usr/lib \
-	export CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
+#	export INSTALL_DIR=${INSTALL_DIR:-"$ORACLE_BASE/install"}
+#	export PATH=$ORACLE_HOME/bin:$ORACLE_HOME/OPatch/:/usr/sbin:$PATH
+#	export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/usr/lib
+#	export CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
 	# Copy binaries
 	# -------------
+	echo "Copy binaries"
 	mkdir -p $INSTALL_DIR/
 	cd $INSTALL_DIR/
 		wget -O $INSTALL_RSP --no-check-certificate $DOWN_URL/template/$INSTALL_RSP-$VERSION
@@ -59,6 +61,7 @@ if [ -f /etc/redhat-release ]; then
 	chmod ug+x $INSTALL_DIR/*.sh
 	chmod ug+x $ORACLE_BASE/*.sh
 	# Download setup files
+	echo "Download setup files"
 	cd $INSTALL_DIR/ && \
 	if [[ ! -z "${INSTALL_FILE_1}" ]]; then
 		if [ ! -f "$INSTALL_FILE_1" ]; then wget --no-check-certificate --progress=bar:force $HOST_DOWN/$INSTALL_FILE_1; fi
@@ -73,17 +76,21 @@ if [ -f /etc/redhat-release ]; then
 		if [ ! -f "$INSTALL_FILE_1" ]; then wget --no-check-certificate --progress=bar:force $HOST_DOWN/$INSTALL_FILE_4; fi
 	fi
 	# Install prepare setup
+	echo "Install prepare setup"
 		sync && \
 		$INSTALL_DIR/$CHECK_SPACE_FILE && \
 		$INSTALL_DIR/$SETUP_LINUX_FILE
 
 	# install gosu
+	echo "install gosu"
 		curl -s $DOWN_URL/gosu_install.sh | bash
 	# Install DB software binaries
+	echo "Install DB software binaries"
 	# su -H -u oracle bash -c '$INSTALL_DIR/$INSTALL_DB_BINARIES_FILE $PRODUCT'
 		gosu oracle $INSTALL_DIR/$INSTALL_DB_BINARIES_FILE $PRODUCT
 
 	# Clean
+	echo "Clean"
 		$ORACLE_BASE/oraInventory/orainstRoot.sh && \
 		$ORACLE_HOME/root.sh && \
 		rm -rf $INSTALL_DIR
