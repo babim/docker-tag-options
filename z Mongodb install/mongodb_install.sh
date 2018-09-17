2369da2778e22fa8424a2aad6ce762b8568555a6
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-17 15:23:00 +0700

    1

--- z Mongodb install/mongodb_install.sh
+++ z Mongodb install/mongodb_install.sh
@@ -25,6 +25,9 @@ echo 'Check OS'
 		[[ -d /var/log/supervisor ]] || mkdir -p /var/log/supervisor/
 		[[ -d /etc/supervisor/conf.d ]] || mkdir -p /etc/supervisor/conf.d/
 	# download sypervisord config
+	FILETEMP=/etc/supervisor/supervisord.conf
+		[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
+		wget --no-check-certificate -O $FILETEMP $DOWN_URL/supervisor/supervisord.conf
 	FILETEMP=/etc/supervisor/conf.d/mongodb.conf
 		[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
 		wget --no-check-certificate -O $FILETEMP $DOWN_URL/supervisor/mongodb.conf
--- z PHP install/php_install.sh
+++ z PHP install/php_install.sh
@@ -27,6 +27,9 @@ if [[ -f /etc/lsb-release ]]; then
 		[[ -d /var/log/supervisor ]] || mkdir -p /var/log/supervisor/
 		[[ -d /etc/supervisor/conf.d ]] || mkdir -p /etc/supervisor/conf.d/
 	# download sypervisord config
+	FILETEMP=/etc/supervisor/supervisord.conf
+		[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
+		wget --no-check-certificate -O $FILETEMP $DOWN_URL/supervisor/supervisord.conf
 	FILETEMP=/etc/supervisor/conf.d/phpfpm-${PHP_VERSION}.conf
 		[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
 		wget --no-check-certificate -O $FILETEMP $DOWN_URL/supervisor/conf.d/phpfpm-${PHP_VERSION}.conf
