daa8b8ff3b62244f590904d4a3c47ce043bb9d5e
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-18 14:06:34 +0700

    1

--- z ElasticStack install/supervisor_elasticsearch.sh
+++ z ElasticStack install/supervisor_elasticsearch.sh
@@ -1,14 +1,14 @@
-	# install
-	apk add --no-cache supervisor
-		# Supervisor config
-			[[ -d /var/log/supervisor ]] || mkdir -p /var/log/supervisor/
-			[[ -d /etc/supervisor/conf.d ]] || mkdir -p /etc/supervisor/conf.d/
-		# download sypervisord config
-		FILETEMP=/etc/supervisor/supervisord.conf
-			[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
-			wget -O $FILETEMP $DOWN_URL/supervisor/supervisord.conf
-		FILETEMP=/etc/supervisord.conf
-			[[ ! -f $FILETEMP ]] || ln -sf $FILETEMP /etc/supervisor/supervisord.conf
-		FILETEMP=/etc/supervisor/conf.d/elasticsearch.conf
-			[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
-			wget -O $FILETEMP $DOWN_URL/supervisor/conf.d/elasticsearch.conf
+	# # install
+	# apk add --no-cache supervisor
+	# 	# Supervisor config
+	# 		[[ -d /var/log/supervisor ]] || mkdir -p /var/log/supervisor/
+	# 		[[ -d /etc/supervisor/conf.d ]] || mkdir -p /etc/supervisor/conf.d/
+	# 	# download sypervisord config
+	# 	FILETEMP=/etc/supervisor/supervisord.conf
+	# 		[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
+	# 		wget -O $FILETEMP $DOWN_URL/supervisor/supervisord.conf
+	# 	FILETEMP=/etc/supervisord.conf
+	# 		[[ ! -f $FILETEMP ]] || ln -sf $FILETEMP /etc/supervisor/supervisord.conf
+	# 	FILETEMP=/etc/supervisor/conf.d/elasticsearch.conf
+	# 		[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
+	# 		wget -O $FILETEMP $DOWN_URL/supervisor/conf.d/elasticsearch.conf
--- z ElasticStack install/supervisor_kibana.sh
+++ z ElasticStack install/supervisor_kibana.sh
@@ -1,14 +1,14 @@
-	# install
-	apk add --no-cache supervisor
-		# Supervisor config
-			[[ -d /var/log/supervisor ]] || mkdir -p /var/log/supervisor/
-			[[ -d /etc/supervisor/conf.d ]] || mkdir -p /etc/supervisor/conf.d/
-		# download sypervisord config
-		FILETEMP=/etc/supervisor/supervisord.conf
-			[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
-			wget -O $FILETEMP --no-check-certificate $DOWN_URL/supervisor/supervisord.conf
-		FILETEMP=/etc/supervisord.conf
-			[[ ! -f $FILETEMP ]] || ln -sf $FILETEMP /etc/supervisor/supervisord.conf
-		FILETEMP=/etc/supervisor/conf.d/kibana.conf
-			[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
-			wget -O $FILETEMP --no-check-certificate $DOWN_URL/supervisor/conf.d/kibana.conf
+	# # install
+	# apk add --no-cache supervisor
+	# 	# Supervisor config
+	# 		[[ -d /var/log/supervisor ]] || mkdir -p /var/log/supervisor/
+	# 		[[ -d /etc/supervisor/conf.d ]] || mkdir -p /etc/supervisor/conf.d/
+	# 	# download sypervisord config
+	# 	FILETEMP=/etc/supervisor/supervisord.conf
+	# 		[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
+	# 		wget -O $FILETEMP --no-check-certificate $DOWN_URL/supervisor/supervisord.conf
+	# 	FILETEMP=/etc/supervisord.conf
+	# 		[[ ! -f $FILETEMP ]] || ln -sf $FILETEMP /etc/supervisor/supervisord.conf
+	# 	FILETEMP=/etc/supervisor/conf.d/kibana.conf
+	# 		[[ ! -f $FILETEMP ]] || rm -f $FILETEMP
+	# 		wget -O $FILETEMP --no-check-certificate $DOWN_URL/supervisor/conf.d/kibana.conf
