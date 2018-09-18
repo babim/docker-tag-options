955a0a1802447caf29d53faec3ec7966bc596bc2
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-09-18 08:25:57 +0700

    1

--- z ElasticStack install/kibana_start.sh
+++ z ElasticStack install/kibana_start.sh
@@ -17,7 +17,7 @@ if [ -f "/option.sh" ]; then /option.sh; fi
 
 # Add kibana as command if needed
 if [[ "$1" == -* ]]; then
-	set -- kibana kibana
+	set -- kibana "$@"
 fi
 
 # Run as user "kibana" if the command is "kibana"
@@ -26,7 +26,7 @@ if [ "$1" = 'kibana' ]; then
 		sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /usr/share/kibana/config/kibana.yml
 	fi
 
-	set -- su-exec kibana /sbin/tini -- kibana
+	set -- su-exec kibana /sbin/tini -- "$@"
 fi
 
 exec "$@"
