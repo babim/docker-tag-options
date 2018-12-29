bde312953691e495be8180a640cc09512a8396fa
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-12-29 18:19:13 +0700

    1

--- z Xubuntu install/startup.sh
+++ z Xubuntu install/startup.sh
@@ -2,7 +2,7 @@
 # option with entrypoint
 if [ -f "/option.sh" ]; then /option.sh; fi
 if [ -f "/start.sh" ]; then /start.sh; fi
-if [ -z "`/root`" ]; then cp -R /etc-start/root/* /root; fi
+if [ -z "/root" ]; then cp -R /etc-start/root/* /root; fi
 rm -rf /tmp/.X*
 USER=root
 HOME=/root
@@ -10,5 +10,6 @@ export USER HOME
     # set password root is root
     SSHPASS=${SSHPASS:-root}
     echo "root:$SSHPASS" | chpasswd
+/vnckey.sh
 vncserver :1
 sleep infinity
