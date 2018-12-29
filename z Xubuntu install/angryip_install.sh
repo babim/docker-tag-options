ba5cdd1ef5fb07ee49d58d16bf9b3eafe34a7202
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-12-29 19:49:32 +0700

    1

--- z Xubuntu install/angryip_install.sh
+++ z Xubuntu install/angryip_install.sh
@@ -3,4 +3,4 @@
 			ca-certificates-java fonts-dejavu-extra java-common libbonobo2-0 \
 			libbonobo2-common libgnome-2-0 libgnome2-common libgnomevfs2-0 \
 			libgnomevfs2-common liborbit-2-0 openjdk-8-jre openjdk-8-jre-headless && \
-			dpkg -i ipscan_$IPSCAN.deb && rm -f ipscan_$IPSCAN.deb
+			dpkg -i ipscan_${IPSCAN}_amd64.deb && rm -f ipscan_${IPSCAN}_amd64.deb
