366fcfe97117152254802c0c5446df96fefabb38
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-12-29 10:46:10 +0700

    1

--- z Xubuntu install/startup.sh
+++ z Xubuntu install/startup.sh
@@ -2,7 +2,7 @@
 # option with entrypoint
 if [ -f "/option.sh" ]; then /option.sh; fi
 if [ -f "/start.sh" ]; then /start.sh; fi
-if [ -z "`/root`" ]; then cp -R /etc-start/root/* /etc/root; fi
+if [ -z "`/root`" ]; then cp -R /etc-start/root/* /root; fi
 rm -rf /tmp/.X*
 USER=root
 HOME=/root
