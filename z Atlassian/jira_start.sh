dbdf8f5365f934949b9ee7f05f7e4f96919fbc72
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-02-22 02:12:29 +0700

    1

--- z Atlassian/jira_start.sh
+++ z Atlassian/jira_start.sh
@@ -8,55 +8,54 @@
 set -e
 
 # set environment
-
+#	export SOFT=${SOFT:-jira}
+#	export SOFT_HOME=${SOFT_HOME:-/var/atlassian/${SOFT}}
+#	export SOFT_INSTALL=${SOFT_INSTALL:-/opt/atlassian/${SOFT}}
 
 # option with entrypoint
 if [ -f "/option.sh" ]; then /option.sh; fi
-	export SOFT=${SOFT:-jira}
-	export SOFT_HOME=${SOFT_HOME:-/var/atlassian/${SOFT}}
-	export SOFT_INSTALL=${SOFT_INSTALL:-/opt/atlassian/${SOFT}}
 
 # visible code
 	if [ "${VISIBLECODE}" = "true" ]; then
-		if [ -z "`ls ${SOFTINSTALL}`" ]; then
-			cp -R /etc-start/jira/* ${SOFTINSTALL}
+		if [ -z "`ls ${SOFT_INSTALL}`" ]; then
+			cp -R /etc-start/jira/* ${SOFT_INSTALL}
 		## set permission path
-			chmod -R 700            "${SOFTINSTALL}/conf"
-			chmod -R 700            "${SOFTINSTALL}/logs"
-			chmod -R 700            "${SOFTINSTALL}/temp"
-			chmod -R 700            "${SOFTINSTALL}/work"
-			chown -R daemon:daemon  "${SOFTINSTALL}/conf"
-			chown -R daemon:daemon  "${SOFTINSTALL}/logs"
-			chown -R daemon:daemon  "${SOFTINSTALL}/temp"
-			chown -R daemon:daemon  "${SOFTINSTALL}/work"
+			chmod -R 700            "${SOFT_INSTALL}/conf"
+			chmod -R 700            "${SOFT_INSTALL}/logs"
+			chmod -R 700            "${SOFT_INSTALL}/temp"
+			chmod -R 700            "${SOFT_INSTALL}/work"
+			chown -R daemon:daemon  "${SOFT_INSTALL}/conf"
+			chown -R daemon:daemon  "${SOFT_INSTALL}/logs"
+			chown -R daemon:daemon  "${SOFT_INSTALL}/temp"
+			chown -R daemon:daemon  "${SOFT_INSTALL}/work"
 		fi
 	fi
 
 # check if the `server.xml` file has been changed since the creation of this
 # Docker image. If the file has been changed the entrypoint script will not
 # perform modifications to the configuration file.
-if [ "$(stat -c "%Y" "${SOFTINSTALL}/conf/server.xml")" -eq "0" ]; then
+if [ "$(stat -c "%Y" "${SOFT_INSTALL}/conf/server.xml")" -eq "0" ]; then
  	if [ -n "${X_PROXY_NAME}" ]; then
-		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${SOFTINSTALL}/conf/server.xml"
+		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${SOFT_INSTALL}/conf/server.xml"
 	fi
 	if [ -n "${X_PROXY_PORT}" ]; then
-		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${SOFTINSTALL}/conf/server.xml"
+		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${SOFT_INSTALL}/conf/server.xml"
 	fi
 	if [ -n "${X_PROXY_SCHEME}" ]; then
-		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${SOFTINSTALL}/conf/server.xml"
+		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${SOFT_INSTALL}/conf/server.xml"
 	fi
 	if [ "${X_PROXY_SCHEME}" = "https" ]; then
-		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "secure" --value "true" "${SOFTINSTALL}/conf/server.xml"
-		xmlstarlet ed --inplace --pf --ps --update '//Connector[@port="8080"]/@redirectPort' --value "${X_PROXY_PORT}" "${SOFTINSTALL}/conf/server.xml"
+		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "secure" --value "true" "${SOFT_INSTALL}/conf/server.xml"
+		xmlstarlet ed --inplace --pf --ps --update '//Connector[@port="8080"]/@redirectPort' --value "${X_PROXY_PORT}" "${SOFT_INSTALL}/conf/server.xml"
 	fi
 	if [ -n "${X_PATH}" ]; then
-		xmlstarlet ed --inplace --pf --ps --update '//Context/@path' --value "${X_PATH}" "${SOFTINSTALL}/conf/server.xml"
+		xmlstarlet ed --inplace --pf --ps --update '//Context/@path' --value "${X_PATH}" "${SOFT_INSTALL}/conf/server.xml"
 	fi
 fi
 
 # visible code
 	if [ "${VISIBLECODE}" = "true" ]; then
-		${SOFTINSTALL}/bin/start-${SOFT}.sh -fg
+		${SOFT_INSTALL}/bin/start-${SOFT}.sh -fg
 	fi
 
 exec "$@"
