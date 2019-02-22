670449e3b71dd4daea670efa673cd900235d01b4
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-02-22 11:02:12 +0700

    1

--- z Atlassian/jira_start.sh
+++ z Atlassian/jira_start.sh
@@ -14,41 +14,45 @@ if [ -f "/option.sh" ]; then /option.sh; fi
 	if [ "${VISIBLECODE}" = "true" ]; then
 		if [ -z "`ls ${SOFT_INSTALL}`" ] || [ ! -d ${SOFT_INSTALL} ]; then
 			if [ ! -d ${SOFT_INSTALL} ]; then mkdir -p ${SOFT_INSTALL}; fi
-				sudo -u root -H sh -c 'cp -R /etc-start/jira/* ${SOFT_INSTALL}'
-				sudo -u root -H sh -c 'mkdir -p "${SOFT_HOME}/caches/indexes"'
+				cp -R /etc-start/jira/* ${SOFT_INSTALL}
+				mkdir -p "${SOFT_HOME}/caches/indexes"
 		## set permission path
-			sudo -u root -H sh -c 'chmod -R 700            "${SOFT_HOME}"'
-			sudo -u root -H sh -c 'chown -R daemon:daemon  "${SOFT_HOME}"'
-			sudo -u root -H sh -c 'chmod -R 700            "${SOFT_INSTALL}/conf"'
-			sudo -u root -H sh -c 'chmod -R 700            "${SOFT_INSTALL}/logs"'
-			sudo -u root -H sh -c 'chmod -R 700            "${SOFT_INSTALL}/temp"'
-			sudo -u root -H sh -c 'chmod -R 700            "${SOFT_INSTALL}/work"'
-			sudo -u root -H sh -c 'chown -R daemon:daemon  "${SOFT_INSTALL}/conf"'
-			sudo -u root -H sh -c 'chown -R daemon:daemon  "${SOFT_INSTALL}/logs"'
-			sudo -u root -H sh -c 'chown -R daemon:daemon  "${SOFT_INSTALL}/temp"'
-			sudo -u root -H sh -c 'chown -R daemon:daemon  "${SOFT_INSTALL}/work"'
+			chmod -R 700            "${SOFT_HOME}"
+			chown -R daemon:daemon  "${SOFT_HOME}"
+			chmod -R 700            "${SOFT_INSTALL}/conf"
+			chmod -R 700            "${SOFT_INSTALL}/logs"
+			chmod -R 700            "${SOFT_INSTALL}/temp"
+			chmod -R 700            "${SOFT_INSTALL}/work"
+			chown -R daemon:daemon  "${SOFT_INSTALL}/conf"
+			chown -R daemon:daemon  "${SOFT_INSTALL}/logs"
+			chown -R daemon:daemon  "${SOFT_INSTALL}/temp"
+			chown -R daemon:daemon  "${SOFT_INSTALL}/work"
 		fi
 	fi
 
 # check if the `server.xml` file has been changed since the creation of this
 # Docker image. If the file has been changed the entrypoint script will not
 # perform modifications to the configuration file.
-if [ "$(stat -c "%Y" "${SOFT_INSTALL}/conf/server.xml")" -eq "0" ]; then
- 	if [ -n "${X_PROXY_NAME}" ]; then
-		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${SOFT_INSTALL}/conf/server.xml"
-	fi
-	if [ -n "${X_PROXY_PORT}" ]; then
-		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${SOFT_INSTALL}/conf/server.xml"
-	fi
-	if [ -n "${X_PROXY_SCHEME}" ]; then
-		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${SOFT_INSTALL}/conf/server.xml"
-	fi
-	if [ "${X_PROXY_SCHEME}" = "https" ]; then
-		xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "secure" --value "true" "${SOFT_INSTALL}/conf/server.xml"
-		xmlstarlet ed --inplace --pf --ps --update '//Connector[@port="8080"]/@redirectPort' --value "${X_PROXY_PORT}" "${SOFT_INSTALL}/conf/server.xml"
-	fi
-	if [ -n "${X_PATH}" ]; then
-		xmlstarlet ed --inplace --pf --ps --update '//Context/@path' --value "${X_PATH}" "${SOFT_INSTALL}/conf/server.xml"
+if [ "${VISIBLECODE}" = "true" ]; then
+	su - daemon
+else
+	if [ "$(stat -c "%Y" "${SOFT_INSTALL}/conf/server.xml")" -eq "0" ]; then
+	 	if [ -n "${X_PROXY_NAME}" ]; then
+			xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${SOFT_INSTALL}/conf/server.xml"
+		fi
+		if [ -n "${X_PROXY_PORT}" ]; then
+			xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${SOFT_INSTALL}/conf/server.xml"
+		fi
+		if [ -n "${X_PROXY_SCHEME}" ]; then
+			xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${SOFT_INSTALL}/conf/server.xml"
+		fi
+		if [ "${X_PROXY_SCHEME}" = "https" ]; then
+			xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "secure" --value "true" "${SOFT_INSTALL}/conf/server.xml"
+			xmlstarlet ed --inplace --pf --ps --update '//Connector[@port="8080"]/@redirectPort' --value "${X_PROXY_PORT}" "${SOFT_INSTALL}/conf/server.xml"
+		fi
+		if [ -n "${X_PATH}" ]; then
+			xmlstarlet ed --inplace --pf --ps --update '//Context/@path' --value "${X_PATH}" "${SOFT_INSTALL}/conf/server.xml"
+		fi
 	fi
 fi
 
