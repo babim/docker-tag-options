c89ce138dcc4d7be0f7f93c150e077491d3129e2
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-03-06 00:02:57 +0700

    1

--- z Xubuntu install/freefilesync_install.sh
+++ z Xubuntu install/freefilesync_install.sh
@@ -1,6 +1,6 @@
-export FREEFILESYNC=${FREEFILESYNC:-10.7_Linux}
-	wget http://media.matmagoc.com/FreeFileSync_$FREEFILESYNC.tar.gz && \
-	tar -xzvpf FreeFileSync_$FREEFILESYNC.tar.gz -C /opt && rm -f FreeFileSync_$FREEFILESYNC.tar.gz
+export FREEFILESYNC=${FREEFILESYNC:-10.9}
+	wget https://freefilesync.org/download/FreeFileSync_${FREEFILESYNC}_Linux.tar.gz && \
+	tar -xzvpf FreeFileSync_${FREEFILESYNC}_Linux.tar.gz -C /opt && rm -f FreeFileSync_${FREEFILESYNC}_Linux.tar.gz
 	mkdir -p /root/Desktop
 	wget -O /root/Desktop/FreeFileSync.desktop http://media.matmagoc.com/FreeFileSync.desktop && \
 	chmod +x /root/Desktop/FreeFileSync.desktop
