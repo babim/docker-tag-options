f5ecb4e9d2e335e973baeb0863e9b1d60ebea8f3
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-03-11 14:09:57 +0700

    fix mysql

--- z Mariadb install/mariadb_repo.sh
+++ z Mariadb install/mariadb_repo.sh
@@ -27,20 +27,46 @@ if [[ -f /etc/lsb-release ]] || [[ -f /etc/debian_version ]]; then
 			add-apt-repository "deb [arch=amd64,i386,ppc64el] http://mirror.truenetwork.ru/mariadb/repo/$MARIADB_MAJOR/debian $OSDEB main"
 		fi
 
-	elif [[ "$TYPESQL" == "mysql" ]] || [[ "$TYPESQL" == "mysql5" ]];then
+	elif [[ "$TYPESQL" == "mysql" ]] || [[ "$TYPESQL" == "mysql5" ]];thenMYSQL_MAJOR
+		apt-get install -y lsb-release gnupg
+		if [[ "$MYSQL_MAJOR" == "5.6" ]];then
+cat <<EOF >keystrokes
+1
+1
+4
+EOF
+		elif [[ "$MYSQL_MAJOR" == "5.7" ]];then
+cat <<EOF >keystrokes
+1
+2
+4
+EOF
+		elif [[ "$MYSQL_MAJOR" == "8.0" ]];then
+cat <<EOF >keystrokes
+1
+3
+4
+EOF
+		fi
+		FILETEMP=mysql-apt-config_0.8.12-1_all.deb
+			wget https://dev.mysql.com/get/$FILETEMP
+			dpkg -i $FILETEMP < keystrokes
+			rm -f $FILETEMP keystrokes
+
+	# code add repo old
 		# add repo Mysql
-		set -ex; \
+		#set -ex; \
 		# gpg: key 5072E1F5: public key "MySQL Release Engineering <mysql-build@oss.oracle.com>" imported
-			key='A4A9406876FCBD3C456770C88C718D3B5072E1F5'; \
-			export GNUPGHOME="$(mktemp -d)"; \
-			gpg --no-tty --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
-			gpg --export "$key" > /etc/apt/trusted.gpg.d/mysql.gpg; \
-			rm -rf "$GNUPGHOME"; \
-			apt-key list > /dev/null
+		#	key='A4A9406876FCBD3C456770C88C718D3B5072E1F5'; \
+		#	export GNUPGHOME="$(mktemp -d)"; \
+		#	gpg --no-tty --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
+		#	gpg --export "$key" > /etc/apt/trusted.gpg.d/mysql.gpg; \
+		#	rm -rf "$GNUPGHOME"; \
+		#	apt-key list > /dev/null
 		# set version
 		#export MYSQL_MAJOR=5.5
 		#export MYSQL_VERSION=5.5.61
-		echo "deb http://repo.mysql.com/apt/debian/ $OSDEB mysql-${MYSQL_MAJOR}" > /etc/apt/sources.list.d/mysql.list
+		#echo "deb http://repo.mysql.com/apt/debian/ $OSDEB mysql-${MYSQL_MAJOR}" > /etc/apt/sources.list.d/mysql.list
 	else
 		echo "Not support your sql"
 	fi
