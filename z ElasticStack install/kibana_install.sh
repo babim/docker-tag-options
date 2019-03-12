1b2faaa410c4b7c36d69b3d35890c7f84ec272de
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-03-12 18:48:35 +0700

    1

--- z ElasticStack install/kibana_install.sh
+++ z ElasticStack install/kibana_install.sh
@@ -35,28 +35,37 @@ if [[ -f /etc/alpine-release ]]; then
 		[[ -f "/usr/share/kibana/node/bin/npm" ]]	&& rm /usr/share/kibana/node/bin/npm
 		ln -s /usr/bin/node /usr/share/kibana/node/bin/node
 		ln -s /usr/bin/npm /usr/share/kibana/node/bin/npm
+		sed -ri "s!^(\#\s*)?(server\.host:).*!\2 '0.0.0.0'!" /usr/share/kibana/config/kibana.yml
+		grep -q "^server\.host: '0.0.0.0'\$" /usr/share/kibana/config/kibana.yml 
+  	# ensure the default configuration is useful when using --link
+		sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 'http://elasticsearch:9200'!" /usr/share/kibana/config/kibana.yml
+		grep -q "^elasticsearch\.url: 'http://elasticsearch:9200'\$" /usr/share/kibana/config/kibana.yml
+	# clean
 		rm -rf /var/cache/apk/*
+		rm -rf /tmp/*
 		[[ -f "/kibana-${KB_VERSION}-linux-${BIT}.tar.gz" ]]	&& /kibana-${KB_VERSION}-linux-${BIT}.tar.gz
 		[[ -f "kibana-${KB_VERSION}-linux-${BIT}.tar.gz" ]]	&& /kibana-${KB_VERSION}-linux-${BIT}.tar.gz
+	elif [ "${KB_VERSION}" == "6.6.0" ] || [ "${KB_VERSION}" == "6.6.1" ] || [ "${KB_VERSION}" == "6.6.2" ] || [ "${KB_VERSION}" == "6.6.3" ] || [ "${KB_VERSION}" == "6.6.4" ]; then
+		echo "no need sed value"
 	else
-  	# the default "server.host" is "localhost" in 5+
-		if [ "${KB_VERSION}" == "6.6.0" ] || [ "${KB_VERSION}" == "6.6.1" ] || [ "${KB_VERSION}" == "6.6.2" ] || [ "${KB_VERSION}" == "6.6.3" ] || [ "${KB_VERSION}" == "6.6.4" ]; then
-			echo "no need sed value"
-		else
-			sed -ri "s!^(\#\s*)?(server\.host:).*!\2 '0.0.0.0'!" /usr/share/kibana/config/kibana.yml
-			grep -q "^server\.host: '0.0.0.0'\$" /usr/share/kibana/config/kibana.yml 
-  		# ensure the default configuration is useful when using --link
-			sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 'http://elasticsearch:9200'!" /usr/share/kibana/config/kibana.yml
-			grep -q "^elasticsearch\.url: 'http://elasticsearch:9200'\$" /usr/share/kibana/config/kibana.yml
-		fi
+		sed -ri "s!^(\#\s*)?(server\.host:).*!\2 '0.0.0.0'!" /usr/share/kibana/config/kibana.yml
+		grep -q "^server\.host: '0.0.0.0'\$" /usr/share/kibana/config/kibana.yml 
+  	# ensure the default configuration is useful when using --link
+		sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 'http://elasticsearch:9200'!" /usr/share/kibana/config/kibana.yml
+		grep -q "^elasticsearch\.url: 'http://elasticsearch:9200'\$" /usr/share/kibana/config/kibana.yml
+	fi
  	 # usr alpine nodejs and not bundled version
-		bundled='NODE="${DIR}/node/bin/node"' \
-		&& apline_node='NODE="/usr/bin/node"' \
-		&& sed -i "s|$bundled|$apline_node|g" /usr/share/kibana/bin/kibana-plugin \
-		&& sed -i "s|$bundled|$apline_node|g" /usr/share/kibana/bin/kibana \
-		&& rm -rf /usr/share/kibana/node \
-		&& chown -R kibana:kibana /usr/share/kibana \
-		&& rm -rf /tmp/*
+		bundled='NODE="${DIR}/node/bin/node"'
+		apline_node='NODE="/usr/bin/node"'
+		sed -i "s|$bundled|$apline_node|g" /usr/share/kibana/bin/kibana-plugin
+		sed -i "s|$bundled|$apline_node|g" /usr/share/kibana/bin/kibana
+		rm -rf /usr/share/kibana/node
+		chown -R kibana:kibana /usr/share/kibana
+	# clean
+		rm -rf /var/cache/apk/*
+		rm -rf /tmp/*
+		[[ -f "/kibana-${KB_VERSION}-linux-${BIT}.tar.gz" ]]	&& /kibana-${KB_VERSION}-linux-${BIT}.tar.gz
+		[[ -f "kibana-${KB_VERSION}-linux-${BIT}.tar.gz" ]]	&& /kibana-${KB_VERSION}-linux-${BIT}.tar.gz
 	fi
 	# download entrypoint files
 		downloadentrypoint() {
