f44403eff40bb370cca7743316b8a8b2093271a0
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-03-19 02:40:39 +0700

    1

---
+++ z ManageEngine/opmanager_install.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#  ____        _     _
+# | __ )  __ _| |__ (_)_ __ ___
+# |  _ \ / _` | '_ \| | '_ ` _ \
+# | |_) | (_| | |_) | | | | | | |
+# |____/ \__,_|_.__/|_|_| |_| |_|
+
+# check permission root
+echo 'Check root'
+if [ "x$(id -u)" != 'x0' ]; then
+	echo 'Error: this script can only be executed by root'
+	exit 1
+fi
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+	echo continue
+else
+	echo 'Your CPU not support!'
+fi
+
+# set environment
+setenvironment() {
+		export SOFT=${SOFT:-opmanager}
+		#export SOFTSUB=${SOFTSUB:-core}
+		export SOFT_HOME=${SOFT:-/opt/ManageEngine/OpManager}	
+
+	# set host download
+		export DOWN_URL="https://raw.githubusercontent.com/babim/docker-tag-options/master/z%20ManageEngine"
+}
+# set command install
+installmanageengine() {
+	if [ ${MACHINE_TYPE} == 'x86_64' ] && [ ${EDITTION} == 'essential'; then
+		wget -O install.bin https://www.manageengine.com/network-monitoring/29809517/ManageEngine_OpManager_64bit.bin
+	elif [ ${MACHINE_TYPE} != 'x86_64' ] && [ ${EDITTION} == 'essential'; then
+		wget -O install.bin https://www.manageengine.com/network-monitoring/29809517/ManageEngine_OpManager.bin
+	elif [ ${MACHINE_TYPE} != 'x86_64' ] && [ ${EDITTION} == 'enterprise'; then
+		wget -O install.bin https://www.manageengine.com/network-monitoring/29809517/ManageEngine_OpManager_${SOFTSUB}_64bit.bin
+	else
+		echo "Not support"
+		exit
+	fi
+		chmod +x install.bin
+cat <<EOF > keystroke
+1
+q
+1
+0
+1
+Admin
+admin@matmagoc.com
+0
+0
+184
+0
+0
+1
+q
+1
+${SOFT_HOME}${SOFTSUB}
+${SOFT_HOME}${SOFTSUB}
+1
+8060
+1
+1
+0
+1
+1
+3
+EOF
+		./install.bin -console < keystroke
+		rm -f install.bin keystroke
+	# prepare data start
+		mkdir /etc-start
+		mv ${SOFT_HOME}${SOFTSUB} /etc-start/${SOFTSUB}
+	# download docker entry
+		FILETEMP=/docker-entrypoint.sh
+		[[ -f $FILETEMP ]] && rm -f $FILETEMP
+			wget -O $FILETEMP --no-check-certificate $DOWN_URL/${SOFT}_start.sh
+		chmod +x $FILETEMP
+}
+cleanmanageengine() {
+	# remove packages
+		wget --no-check-certificate -O - $DOWN_URL/${SOFT}_clean.sh | bash
+}
+
+# install by OS
+echo 'Check OS'
+# OS - alpine linux
+if [[ -f /etc/alpine-release ]]; then
+    echo "Not support your OS"
+    exit
+# OS - ubuntu debian
+elif [[ -f /etc/lsb-release ]] || [[ -f /etc/debian_version ]]; then
+	setenvironment
+	installmanageengine
+	cleanmanageengine
+# OS - redhat
+elif [[ -f /etc/redhat-release ]]; then
+	setenvironment
+	installmanageengine
+	cleanmanageengine
+# OS - other
+else
+    echo "Not support your OS"
+    exit
+fi
---
+++ z ManageEngine/opmanager_start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#  ____        _     _
+# | __ )  __ _| |__ (_)_ __ ___
+# |  _ \ / _` | '_ \| | '_ ` _ \
+# | |_) | (_| | |_) | | | | | | |
+# |____/ \__,_|_.__/|_|_| |_| |_|
+
+set -e
+
+# option with entrypoint
+if [ -f "/option.sh" ]; then /option.sh; fi
+
+	echo "check path and install"
+	if [ -z "`ls ${SOFT_HOME}`" ]; then
+		if [ ! -d ${SOFT_HOME} ]; then mkdir -p ${SOFT_HOME}; fi
+			cp -R /etc-start/${SOFT}/* ${SOFT_HOME}
+
+# Run
+cd ${SOFT_HOME}${SOFTSUB}/bin
+./run.sh
