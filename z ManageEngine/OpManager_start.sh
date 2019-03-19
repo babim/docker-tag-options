3e4492f78aa71a8daf0772700109ba1e462de1b8
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-03-20 02:37:17 +0700

    1

--- z ManageEngine/OpManager_start.sh
+++ z ManageEngine/OpManager_start.sh
@@ -13,6 +13,7 @@ if [ -f "/option.sh" ]; then /option.sh; fi
 	echo "check path and install"
 	if [ ! -z "`ls ${SOFT_HOME}`" ]; then
 		cp -R /start/* ${SOFT_HOME}
+		chown -R postgres:postgres ${SOFT_HOME}/pgsql
 	fi
 # Run
 cd ${SOFT_HOME}/bin
