3bc081eb2cdd506c37701ce639d593fb781885a3
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-03-21 05:07:46 +0700

    1

---
+++ z ManageEngine/KeyManager_start.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#  ____        _     _
+# | __ )  __ _| |__ (_)_ __ ___
+# |  _ \ / _` | '_ \| | '_ ` _ \
+# | |_) | (_| | |_) | | | | | | |
+# |____/ \__,_|_.__/|_|_| |_| |_|
+
+set -e
+
+# check permission root
+echo 'Check root'
+if [[ "x$(id -u)" != 'x0' ]]; then
+	echo 'Error: this script can only be executed by root'
+	exit 1
+fi
+MACHINE_TYPE=`uname -m`
+if [[ ${MACHINE_TYPE} == 'x86_64' ]]; then
+	echo x64
+else
+	echo x86
+fi
+
+# set environment
+setenvironment() {
+		export SOFT=${SOFT:-KeyManager}
+		#export SOFTSUB=${SOFTSUB:-core}
+		export SOFT_HOME=${SOFT_HOME:-/opt/ManageEngine/KeyManager}
+		#export EDITTION=${EDITTION:-essential}
+
+	# set host download
+		export DOWN_URL="https://raw.githubusercontent.com/babim/docker-tag-options/master/z%20ManageEngine"
+}
+# set command install
+installmanageengine() {
+keystroke() {
+cat <<EOF > keystroke
+
+
+
+
+
+
+
+
+
+
+
+
+y
+admin
+0
+admin@matmagoc.com
+0
+252
+1
+/opt/ManageEngine
+y
+
+
+
+
+EOF
+}
+	echo "Download and install"
+	if [[ ${MACHINE_TYPE} == 'x86_64' ]]; then
+		keystroke
+		if [[ ${FIXED} == 'true' ]]; then
+			wget -O install.bin http://media.matmagoc.com/ManageEngine/ManageEngine_KeyManagerPlus_64bit.bin
+		else
+			wget -O install.bin https://www.manageengine.com/key-manager/97531/ManageEngine_KeyManagerPlus_64bit.bin
+		fi
+	elif [[ ${MACHINE_TYPE} != 'x86_64' ]]; then
+		opmanagerkeystroke
+		if [[ ${FIXED} == 'true' ]]; then
+			wget -O install.bin http://media.matmagoc.com/ManageEngine/ManageEngine_KeyManagerPlus.bin
+		else
+			wget -O install.bin https://www.manageengine.com/key-manager/97531/ManageEngine_KeyManagerPlus.bin
+		fi
+	else
+		echo "Not support please edit and rebuild"
+		exit 1
+	fi
+	echo "Install"
+		chmod +x install.bin
+		./install.bin -i console < keystroke
+	# remove install files
+		rm -f install.bin keystroke
+}
+
+# option with entrypoint
+if [ -f "/option.sh" ]; then /option.sh; fi
+
+	echo "check path and install"
+	if [[ -z "`ls ${SOFT_HOME}`" ]] || [[ ! -d "${SOFT_HOME}" ]]; then
+#		rsync -arvpz --numeric-ids /start/ ${SOFT_HOME}
+	# install manage engine
+		setenvironment
+		installmanageengine
+	fi
+# Run
+cd ${SOFT_HOME}/bin
+./run.sh
