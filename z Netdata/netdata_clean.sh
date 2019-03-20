e7ddfca709f803567dad319ec41dff19eca4e423
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-03-21 00:13:53 +0700

    1

--- z Netdata/netdata_clean.sh
+++ z Netdata/netdata_clean.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#  ____        _     _
+# | __ )  __ _| |__ (_)_ __ ___
+# |  _ \ / _` | '_ \| | '_ ` _ \
+# | |_) | (_| | |_) | | | | | | |
+# |____/ \__,_|_.__/|_|_| |_| |_|
+
 export UNINSTALL="libmnl-dev gcc make git autoconf automake wget"
 
  	if [[ -f /etc/redhat-release ]]; then
