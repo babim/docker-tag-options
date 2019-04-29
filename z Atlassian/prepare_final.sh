960aba93f04e4854cd310a1373e9ee08c06b378a
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-04-29 15:05:27 +0700

    1

--- z Atlassian/prepare_final.sh
+++ z Atlassian/prepare_final.sh
@@ -16,17 +16,17 @@
 		$download_save $FILETEMP $DOWN_URL/${SOFT}_fixed.sh
 		$download_save ${SOFT_INSTALL}/agent.jar http://media.matmagoc.com/atlassian-agent.jar
 		if [[ -f  "${SOFT_INSTALL}/bin/start-${SOFT}.sh" ]]; then
-			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/bin/start-${SOFT}.sh"
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${JAVA_OPTS}"' >> "${SOFT_INSTALL}/bin/start-${SOFT}.sh"
 		elif [[ -f  "${SOFT_INSTALL}/bin/start_${SOFT}.sh" ]]; then
-			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/bin/start_${SOFT}.sh"
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${JAVA_OPTS}"' >> "${SOFT_INSTALL}/bin/start_${SOFT}.sh"
 		elif [[ -f  "${SOFT_INSTALL}/start-${SOFT}.sh" ]]; then
-			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/start-${SOFT}.sh"
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${JAVA_OPTS}"' >> "${SOFT_INSTALL}/start-${SOFT}.sh"
 		elif [[ -f  "${SOFT_INSTALL}/start_${SOFT}.sh" ]]; then
-			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/start_${SOFT}.sh"
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${JAVA_OPTS}"' >> "${SOFT_INSTALL}/start_${SOFT}.sh"
 		elif [[ -f  "${SOFT_INSTALL}/bin/start.sh" ]]; then
-			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/bin/start.sh"
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${JAVA_OPTS}"' >> "${SOFT_INSTALL}/bin/start.sh"
 		elif [[ -f  "${SOFT_INSTALL}/start.sh" ]]; then
-			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/start.sh"
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${JAVA_OPTS}"' >> "${SOFT_INSTALL}/start.sh"
 		fi
 	else
 		$download_save $FILETEMP $DOWN_URL/${SOFT}_start.sh
