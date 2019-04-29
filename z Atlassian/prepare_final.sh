5c94ef101ae14172d8acd4f6fda16ddf807f356c
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-04-29 14:57:31 +0700

    1

--- z Atlassian/prepare_final.sh
+++ z Atlassian/prepare_final.sh
@@ -14,9 +14,19 @@
 # visible code
 	if check_value_true "${VISIBLECODE}"; then
 		$download_save $FILETEMP $DOWN_URL/${SOFT}_fixed.sh
-		if [[ "${SOFT}" == "confluence" || "${SOFT}" == "jira" ]]; then
-			$download_save ${SOFT_INSTALL}/agent.jar http://media.matmagoc.com/atlassian-agent.jar
-			echo 'export CATALINA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> ${SOFT_INSTALL}/bin/setenv.sh
+		$download_save ${SOFT_INSTALL}/agent.jar http://media.matmagoc.com/atlassian-agent.jar
+		if [[ -f  "${SOFT_INSTALL}/bin/start-${SOFT}.sh" ]]; then
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/bin/start-${SOFT}.sh"
+		elif [[ -f  "${SOFT_INSTALL}/bin/start_${SOFT}.sh" ]]; then
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/bin/start_${SOFT}.sh"
+		elif [[ -f  "${SOFT_INSTALL}/start-${SOFT}.sh" ]]; then
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/start-${SOFT}.sh"
+		elif [[ -f  "${SOFT_INSTALL}/start_${SOFT}.sh" ]]; then
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/start_${SOFT}.sh"
+		elif [[ -f  "${SOFT_INSTALL}/bin/start.sh" ]]; then
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/bin/start.sh"
+		elif [[ -f  "${SOFT_INSTALL}/start.sh" ]]; then
+			echo 'JAVA_OPTS="-javaagent:${SOFT_INSTALL}/agent.jar ${CATALINA_OPTS}"' >> "${SOFT_INSTALL}/start.sh"
 		fi
 	else
 		$download_save $FILETEMP $DOWN_URL/${SOFT}_start.sh
