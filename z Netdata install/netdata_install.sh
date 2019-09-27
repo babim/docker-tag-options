5a97749bf705e84e8a9abd3d0ce04cafd6b2138b
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-09-27 09:05:43 +0700

    1

--- z Netdata install/netdata_install.sh
+++ z Netdata install/netdata_install.sh
@@ -61,10 +61,8 @@ installnetdata() {
 	cd /
 	remove_filefolder /netdata.git
 	# prepare data
-	if check_folder /etc/netdata; then
-		create_folder /etc-start
-		dircopy /etc/netdata /etc-start/
-	fi
+	create_folder /etc-start
+	dircopy /etc/netdata /etc-start/
 }
 # download docker entrypoint
 downloadentry() {
