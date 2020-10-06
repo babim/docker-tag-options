55622842fe384865acf00068a08828d440f9fd08
Duc Anh Babim <ducanh.babim@yahoo.com>
2020-10-06 12:36:11 +0700

    1

--- z PHP install/apache_install.sh
+++ z PHP install/apache_install.sh
@@ -48,7 +48,7 @@ setenvironment() {
 	if has_value "${FONT}" && ! check_value_false "${FONT}"; then
 		FILETEMP=truetype.zip
 			$download_save $FILETEMP http://file.matmagoc.com/$FILETEMP
-		rm -rf /usr/share/fonts/truetype
+		rm -rf /usr/share/fonts/truetype && mkdir -p /usr/share/fonts/truetype
 			install_package_run unzip
 			unzip_extract $FILETEMP /usr/share/fonts/truetype
 	fi
