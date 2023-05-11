eee97be0a56178257e0aa33f57d73852048391c0
Duc Anh Babim <babim@matmagoc.com>
2023-05-11 21:44:48 +0700

    1

--- z Xubuntu install/xubuntu_install.sh
+++ z Xubuntu install/xubuntu_install.sh
@@ -61,9 +61,9 @@ if [[ -f /etc/lsb-release ]]; then
 		debian_add_repo nilarimogard/webupd8
 		debian_add_repo_key http://deb.opera.com/archive.key && echo "deb http://deb.opera.com/opera-stable/ stable non-free" >> /etc/apt/sources.list.d/opera.list
 		debian_add_repo_key https://dl-ssl.google.com/linux/linux_signing_key.pub && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+		debian_add_repo_key https://packages.microsoft.com/keys/microsoft.asc && echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" >> /etc/apt/sources.list.d/edge.list
 		debian_add_repo teejee2008/ppa
 		#debian_add_repo webupd8team/java
-		debian_add_repo_key https://packages.microsoft.com/keys/microsoft.asc && echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" >> /etc/apt/sources.list.d/edge.list
 	# install GUI
 		install_package xubuntu-desktop
 	# install app 
